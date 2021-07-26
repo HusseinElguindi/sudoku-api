dbacdb21679fd679d221b8a88c07c1bb13a54bf7
Hussein Elguindi <34726859+HusseinElguindi@users.noreply.github.com>
2021-07-26 16:18:21 -0400

    db: document store and puzzle tests

--- db/puzzle_test.go
+++ db/puzzle_test.go
@@ -9,24 +9,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// createRandomPuzzle generates a random puzzle and inserts it into the testQueries database.
+// The inserted puzzle is returned and guaranteed to be valid. Otherwise, the test (t) is failed.
 func createRandomPuzzle(t *testing.T) Puzzle {
+	// Generate a random string of 25 ASCII letters
 	arrayStr := gofakeit.LetterN(25)
+	// Insert puzzle into db
 	puzzle, err := testQueries.CreatePuzzle(context.Background(), arrayStr)
 
+	// Validate the inserted values
 	require.NoError(t, err)
 	require.NotEmpty(t, puzzle)
-
 	require.NotZero(t, puzzle.ID)
+
+	// Compare the inserted values with the original values
 	require.Equal(t, puzzle.ArrayStr, arrayStr)
 	require.WithinDuration(t, puzzle.CreatedAt, time.Now(), time.Second)
 
 	return puzzle
 }
 
-func TestCreatePuzzle(t *testing.T) {
-	createRandomPuzzle(t)
-}
+// TestCreatePuzzle inserts a random puzzle into the global test db, failing the test on any errors
+// or unexpected results.
+func TestCreatePuzzle(t *testing.T) { createRandomPuzzle(t) }
 
+// TestGetPuzzleByID inserts a random puzzle into the db and attempts to query it by its ID.
+// The test fails if no puzzle was returned or it does not match the inserted puzzle.
 func TestGetPuzzleByID(t *testing.T) {
 	insertPuzzle := createRandomPuzzle(t)
 	getPuzzle, err := testQueries.GetPuzzleByID(context.Background(), insertPuzzle.ID)
@@ -35,6 +43,8 @@ func TestGetPuzzleByID(t *testing.T) {
 	require.Equal(t, insertPuzzle, getPuzzle)
 }
 
+// TestGetPuzzleByArrayStr inserts a random puzzle into the db and attempts to query it by its ArrayStr.
+// The test fails if no puzzle was returned or it does not match the inserted puzzle.
 func TestGetPuzzleByArrayStr(t *testing.T) {
 	insertPuzzle := createRandomPuzzle(t)
 	getPuzzle, err := testQueries.GetPuzzleByArrayStr(context.Background(), insertPuzzle.ArrayStr)
--- db/store.go
+++ db/store.go
@@ -1,3 +1,4 @@
+// Package db implements a database interface for storing user and puzzle data.
 package db
 
 import (
@@ -6,11 +7,13 @@ import (
 	"fmt"
 )
 
+// Store represents a datastore object with a SQL database, for easier execution of transactions.
 type Store struct {
 	*Queries
 	db *sql.DB
 }
 
+// NewStore returns a reference to a Store object, constructed with db.
 func NewStore(db *sql.DB) *Store {
 	return &Store{
 		Queries: New(db),
@@ -18,19 +21,25 @@ func NewStore(db *sql.DB) *Store {
 	}
 }
 
+// execTx executes txFunc (and its queries) as one atomic transaction, with context.
 func (s *Store) execTx(ctx context.Context, txFunc func(*Queries) error) error {
+	// Start a transaction
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 
+	// Execute the passed queries func as a transaction
 	queries := New(tx)
 	if err := txFunc(queries); err != nil {
+		// Error occured, rollback changes
 		if rbError := tx.Rollback(); rbError != nil {
+			// Rollback error occured (in addition to a tx error)
 			return fmt.Errorf("rollback err: %w, tx err: %v", rbError, err)
 		}
 		return err
 	}
 
+	// Commit transaction
 	return tx.Commit()
 }
